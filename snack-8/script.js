e2df34577b7cabcd986d7c322c2f599baf003775
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-30 19:09:26 +0100

    add snack 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -22,6 +22,8 @@ const students = [
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
 
-// const findStudents = students.find ((Element) => Element.name === "Marco Lanci")
-// console.log (findStudents)
+ const findStudents = students.find ((Element) => Element.name === "Marco Lanci")
+ console.log (findStudents.class)
+
+
 
